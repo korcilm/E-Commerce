e218de516983a74f4f3476685e81023a60dfc047
korcilm <58364191+korcilm@users.noreply.github.com>
2021-04-01 16:30:00 +0300

    Add project files.

---
+++ API.Data/DataContext/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Data.DataContext
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        
+    }
+}
---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "Get All";
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Get by id";
+        }
+    }
+}
