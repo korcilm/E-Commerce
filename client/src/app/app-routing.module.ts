d45077c4387f5cb3bf9edc9c501505533315db2d
korcilm <58364191+korcilm@users.noreply.github.com>
2021-05-04 23:28:56 +0300

    error handling

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  {path:"",component:HomeComponent},
-  {path:"shop",component:ShopComponent},
-  {path:"shop/:id",component:ProductDetailsComponent},
-  {path:"**",redirectTo:"",pathMatch:"full"},
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Anasayfa' } },
+  {
+    path: 'test-error',
+    component: TestErrorComponent,
+    data: { breadcrumb: 'Test Error' },
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not Found' },
+  },
+  { path: 'shop', component: ShopComponent, data: { breadcrumb: 'Shop' } },
+  {
+    path: 'shop/:id',
+    component: ProductDetailsComponent,
+    data: { breadcrumb: { alias: 'shopDetail' } },
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/IProduct';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class ProductDetailsComponent implements OnInit {
 
   product:IProduct;
 
-  constructor(private shopService:ShopService,private activateRoute:ActivatedRoute) { }
+  constructor(private shopService:ShopService,private activateRoute:ActivatedRoute, private breadcrumbService : BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -21,6 +22,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get("id")).subscribe(pro=>{
       this.product=pro;
+      this.breadcrumbService.set('@shopDetail', this.product.name);
     }),console.error();
     
   }
